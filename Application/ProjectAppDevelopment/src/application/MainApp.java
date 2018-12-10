096c7d050dbcf73a165d4ebaa7ce10fa90d79159
binduvr <bindu.vanraak@gmail.com>
2018-12-10 21:02:52 +0100

    Added a dialog to show assumptions

--- Application/ProjectAppDevelopment/src/application/MainApp.java
+++ Application/ProjectAppDevelopment/src/application/MainApp.java
@@ -5,21 +5,23 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import view.StatisticsViewController;
 
 public class MainApp extends Application {
 	
-	private Stage primaryStage;
+	private static Stage primaryStage;
 	private BorderPane rootLayout;
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("ProjectAP v1.0");
+		MainApp.primaryStage = primaryStage;
+		MainApp.primaryStage.setTitle("ProjectAP v1.0");
 		
 		initRootLayout();
-		
+		initStatisticsView();
 	}
 	
 	public void initRootLayout() {
@@ -37,10 +39,23 @@ public class MainApp extends Application {
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 	
-	public Stage getPrimaryStage() {
+	public void initStatisticsView() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("../view/StatisticsView.fxml"));
+			AnchorPane content = (AnchorPane) loader.load();
+			
+			rootLayout.setCenter(content);
+			StatisticsViewController controller = loader.getController();
+			controller.setMainApp(this);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Stage getPrimaryStage() {
 		return primaryStage;
 	}
 	public static void main(String[] args) {
---
+++ Application/ProjectAppDevelopment/src/view/StatisticsViewController.java
@@ -0,0 +1,10 @@
+package view;
+
+import application.MainApp;
+
+public class StatisticsViewController {
+	private MainApp mainApp;
+	public void setMainApp(MainApp mainApp) {
+		this.mainApp = mainApp;
+	}
+}
